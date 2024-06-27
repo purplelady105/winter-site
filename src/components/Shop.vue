75848414b3f3fe24a40efbcf18fb65349e32eba3
Allison Broughton <purple.lady105@gmail.com>
2024-06-27 18:14:39 -0400

    Update book cover image. Update main page with new info. Add link to B&N to purchase. Fix Tangerine Google import. Remove cursive font.

---
+++ src/components/Shop.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-container class="overflow-auto">
+        <v-card dark color="transparent">
+            <v-row class="text-center">
+                <v-col class="mb-4 accent--text">
+                    <h2 class="font-weight-bold fantasyFont mb-3">Shop</h2>
+                    <p class="subheading font-weight-regular">
+                        The Dark Wing is available now at Barnes & Noble!
+                        <v-btn>
+                            <a href="https://www.barnesandnoble.com/w/the-dark-wing-winter-hartdegen/1145846118" target="_blank">Buy from Barnes & Noble Now!
+                            </a>
+                        </v-btn>
+                    </p>
+                </v-col>
+            </v-row>
+        </v-card>
+    </v-container>
+</template>
+<script>
+export default {
+    name: "Contact",
+
+    data: () => ({
+        //
+    }),
+};
+</script>
+<style scoped>
+a {
+    color: #FFFCD8 !important;
+    text-decoration: none !important;
+}
+
+a:hover {
+    text-decoration: underline !important;
+    color: #3E0804;
+}
+</style>
